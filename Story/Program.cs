5d7b8115cd2847fd20e09464e7f7ce685fa1dfb2
Azsrs <farzanehfrank@icloud.com>
2025-09-04 23:24:18 +0200

    Progress snart klar typ ish

--- Story/Program.cs
+++ Story/Program.cs
@@ -40,16 +40,67 @@ while (Attack1 != "dagger" && Attack1 != "block") //ifall invalid svar
     Console.WriteLine("Do you block or do you use your dagger? Or might it be time to give up");
     Attack1 = Console.ReadLine().ToLower();
 }
-if (Attack1 == "dagger") //ond, bråd död
+if (Attack1 == "dagger") //Om man väljer dagger
 {
     Console.WriteLine("You reach for your blade, and grip it tight in your right hand. Your opponent winds up a haymaker with his right fist aimed straight at your face, using the other hand to guard his face");
     Console.ReadLine();
-    Console.WriteLine("You stab toward your opponents' stomach, praying this will somehow end peacefully./n Lost in thought, you don't notice the opponents' block has shifted down to his stomach.");
+    Console.WriteLine("You stab toward your opponents' stomach, praying this will somehow end peacefully.\n Lost in thought, you don't notice the opponents' block has shifted down to his stomach.");
     Console.ReadLine();
     Console.WriteLine("Your dagger stabs only his arm and you feel a sudden slam to your head");
     Console.ReadLine();
     Console.WriteLine("Ears ringing, lights blaring.. shit... its really over huh?");
     Console.ReadLine();
+    Console.WriteLine("Give up?");
+
+    static void EndGame() //method för att snabbt kunna döda spelaren+stänga programmet 
+    {
+        Console.WriteLine("Flatline.. Game Over");
+        Console.ReadLine();
+        Environment.Exit(0);
+    }
+
+    string GiveUp = Console.ReadLine().ToLower();
+    int InvalidAnswer = 0; //jag deklarerar int innan for-loopen för att kunna använda samma värde i while-loopen
+    if (GiveUp != "yes" && GiveUp != "no" && GiveUp != "fight")
+    {
+        for (InvalidAnswer = 0; InvalidAnswer < 3; InvalidAnswer++) //tre försök att svara rätt annars gameover
+        {
+            Console.WriteLine("...");
+            GiveUp = Console.ReadLine().ToLower();
+            if (GiveUp == "yes" || GiveUp == "no" || GiveUp == "fight")
+            {
+                break;
+            }
+        }
+
+    }
+    if (InvalidAnswer == 3 || GiveUp == "yes")
+    {
+        EndGame();
+    }
+    if (GiveUp == "no")
+    {
+        Console.WriteLine($"You can't give up. At least not yet!\n You try to pull yourself up, the crowd chanting your name {name}, {name}! Maybe all hope is not lost!");
+        Console.ReadLine();
+        Console.WriteLine("SLAM");
+        Console.ReadLine();
+        EndGame();
+    }
+    if (GiveUp == "Fight")
+    {
+        Console.WriteLine("....");
+        Console.ReadLine();
+        Console.WriteLine("Something within you just doesn't let up, huh?");
+        Console.ReadLine();
+        Console.WriteLine("Do or die now, get up and finish this. It's you or him. \n Do you want to try and stab again? Or do you try and tackle him?");
+        string FightBack = Console.ReadLine().ToLower();
+        while (FightBack != "stab" && FightBack != "tackle")
+        {
+            Console.WriteLine("Stab or tackle?");
+            FightBack = Console.ReadLine();
+        }
+        
+    }
 }
 else if (Attack1 == "block")
 {
@@ -59,4 +110,6 @@ else if (Attack1 == "block")
     string pocket = Console.ReadLine().ToLower();
 
 }
+
+
 Console.ReadLine();
--- Story/obj/Debug/net9.0/Story.AssemblyInfo.cs
+++ Story/obj/Debug/net9.0/Story.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Story")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7aa52bd2e06ed9ef829f8e15fedf05abfe91539d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Story")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Story")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
